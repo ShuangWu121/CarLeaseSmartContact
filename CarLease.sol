8350317637b904063fade337f1760acedb278846
ShuangWu121 <47602565+ShuangWu121@users.noreply.github.com>
2020-11-02 13:00:48 +0100

    Create CarLease.sol

---
+++ CarLease.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.7.0;
+
+contract Purchase {
+    uint public value;
+    address payable public seller;
+    address payable public buyer;   
+}
