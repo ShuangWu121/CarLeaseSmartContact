6bea18556602c9348c21e6654b850ff3684a9ebe
ShuangWu121 <47602565+ShuangWu121@users.noreply.github.com>
2020-11-05 09:09:51 +0100

    Update CarLease.sol

--- CarLease.sol
+++ CarLease.sol
@@ -1,8 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.7.0;
 
-contract Purchase {
+contract CarLease {
     uint public value;
     address payable public seller;
     address payable public buyer;   
+    
+     modifier onlyBilBoyd() { // Modifier
+        require(
+            msg.sender == BilBoyd,
+            "Only car company BilBoyd can call this."
+        );
+        _;
+    }
+    
+    constructor() payable {
+        BilBoyd = msg.sender;
+        //value = msg.value / 2;
+        //require((2 * value) == msg.value, "Value has to be even.");
+    }
+
 }
