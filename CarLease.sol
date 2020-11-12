60a4ace476b646b4259fed5ecb429ce06fec5724
ShuangWu121 <47602565+ShuangWu121@users.noreply.github.com>
2020-11-12 14:44:14 +0100

    Update CarLease.sol

--- CarLease.sol
+++ CarLease.sol
@@ -5,8 +5,7 @@ pragma solidity ^0.7.0;
 contract CarLease {
     address payable public BilBoyd;
     
-    enum Plan {Plan1, Plan2}
-    enum Termination {Terminate, BuyCar,Extend} 
+
     struct Customer { // Struct
         address payable CustomerAddress;
         uint Deposit;
@@ -15,19 +14,24 @@ contract CarLease {
         uint Duration;
         uint CarValue;
         uint WeeklyPay;
-        Plan ChoosenPlan;
         uint StartTime;
         uint Payments;
+        uint Loyalty;
     }
     
     Customer public customer;
     
+    struct Car {
+        uint CarValue;
+        uint MileCap;
+    }
+    
+    Car[3] car;
     
     
-    enum State { Created, Active, Inactive }
-    enum TerminationState {Extend,BuyCar,Terminate}
+ 
+    enum TerminationState {Extend,BuyCar,Terminate,NewSign}
     // The state variable has a default value of the first member, `State.created`
-    State public state;
      
     
     modifier onlyBilBoyd() { // Modifier
@@ -48,7 +52,22 @@ contract CarLease {
     
     constructor() payable {
         BilBoyd = msg.sender;
-        state=State.Created;
+        car[0].CarValue=20000;
+        car[0].MileCap=2000;
+    
+        car[1].CarValue=30000;
+        car[1].MileCap=3000;
+    
+        car[2].CarValue=40000;
+        car[2].MileCap=4000;
+    }
+    
+    
+    function BilBoydCustomerInfo(uint8 car_index, uint8 experience) public onlyBilBoyd {
+        customer.CarValue = car[car_index].CarValue;
+        customer.Experience = experience;
+        customer.MileCap = car[car_index].MileCap; 
+        customer.Loyalty=1;
     }
     
     
@@ -60,85 +79,88 @@ contract CarLease {
        3. Duration
        4. StartDate
     */
-    function Registeration(uint8 carValue, Plan plan, uint experience, uint mileCap, uint contractDuration) 
-        public onlyBilBoyd payable {
+    function Registeration(uint contractDuration) 
+        public payable {
     
-        uint requredDeposit=(carValue+mileCap)*15/100; //Formula for deposit goes here
+        uint requredDeposit=(customer.CarValue+customer.MileCap)*15/100; //Formula for deposit goes here
         
         require(msg.value>=requredDeposit, "Deposit not enough.");
         
-        if(msg.value > requredDeposit)
-            customer.CustomerAddress.transfer(msg.value - requredDeposit);
-        customer.Payments += 1;
+       // if(msg.value > requredDeposit)
+        //    customer.CustomerAddress.transfer(msg.value - requredDeposit);
+    
         
-        customer.Experience = experience;
-        customer.MileCap = mileCap;
         customer.Duration = contractDuration;
-        customer.CarValue = carValue;
         customer.CustomerAddress = msg.sender;
-        customer.Deposit = msg.value;
-        customer.ChoosenPlan = plan;
+        customer.Deposit = requredDeposit;
+
         
-        customer.WeeklyPay = (carValue + mileCap)/(contractDuration * experience * 7000);
-        state = State.Active;
+        customer.WeeklyPay = (customer.CarValue + customer.MileCap)/(contractDuration * customer.Experience * 7000 * customer.Loyalty);
         customer.StartTime = block.timestamp;
         customer.Payments=0;
+        customer.Payments += 1;
     }
     
-    function Time() public {
-       // createTime = now;
-    }
+
     
-    function ViewLeasePlan() public view returns(string memory,string memory) {
-        string memory Plan1="Plan 1 : WeeklyPay CarValue* 0.001";
-        string memory Plan2="Plan 2 : WeeklyPay CarValue*0.0001+Mile*30";
-      return (Plan1,Plan2);
-    }
-    
-    function ChoosePlan(uint8 n) public returns(Plan) {
-    
-      return customer.ChoosenPlan;
-    }
     
     function WeeklyPay() public onlyCustomer payable {
         require(msg.value >= customer.WeeklyPay,"Its not enough");
         
-        if(msg.value > customer.WeeklyPay)
-            customer.CustomerAddress.transfer(msg.value-customer.WeeklyPay);
+      //  if(msg.value > customer.WeeklyPay)
+        //    customer.CustomerAddress.transfer(msg.value-customer.WeeklyPay);
         customer.Payments += 1;
+        BilBoyd.transfer(customer.WeeklyPay);
     }
     
-    function CheckWeeklyPay() public onlyBilBoyd {
-        uint expectedPayments = (block.timestamp-customer.StartTime)/1 weeks; //Formula to get time since start of contract
-        if(expectedPayments -customer.Payments > 4)
-            customer.Payments += 1;//Remove. Only to fix compile error with if
-           
-            //TODO:
-            //terminate contract if deposit is empty
-        
-    }
+   
     
-    function TerminateContract(TerminationState termination) public onlyBilBoyd {
+    function TerminateContract() public onlyBilBoyd {
         require(block.timestamp > customer.StartTime + customer.Duration);
         require(
-            customer.Payments < (block.timestamp-customer.StartTime + 3)/1 weeks
+            customer.Payments < (block.timestamp-customer.StartTime )/1 weeks +3
         );
         selfdestruct(BilBoyd);
-
     }
     
-    function TerminateChoice(TerminationState termination) public onlyCustomer returns(string memory) {
-        require(block.timestamp > customer.StartTime + customer.Duration);
+    function TerminateChoice(TerminationState termination, uint NewDuration, uint NewCar) public onlyCustomer payable returns(string memory) {
+        
+        //one week before original contract ends.
+        require(block.timestamp > customer.StartTime + customer.Duration* 1 weeks-1 && block.timestamp <customer.StartTime + customer.Duration* 1 weeks);
+        
+        //Extend the contract
         if(termination==TerminationState.Extend){
-            customer.Payments = (customer.CarValue + customer.MileCap)/(customer.Duration * customer.Experience * 7000 * 2); 
+            customer.Duration =(customer.Duration + 52 weeks); 
+            customer.Loyalty+=1;
             return "Extend successfully"; 
             
-        }   // Recalculated weekly payment with 2 as loyalty parameter
-        else if(termination==TerminationState.BuyCar){
-            customer.Payments = customer.CarValue - customer.Deposit; // payment should be eqaul to car value - paid deposit
-            return 'Bought successfully'
-        }
-      //  else if(termination==TerminationState.Terminate)
-         //   customer.Payments = selfdestruct(customer.CustomerAddress); //https://solidity-by-example.org/0.6/hacks/self-destruct/ example ıs here 
+            
+        }  
         
+        else if(termination==TerminationState.BuyCar){
+            uint Car;
+            Car=customer.CarValue - customer.Deposit;
+            require(msg.value>Car,"Not enough to buy the car"); // payment should be eqaul to car value - paid deposit
+            BilBoyd.transfer(Car);
+            customer.Duration=block.timestamp-customer.StartTime;
+           // if(msg.value > customer.CarValue - customer.Deposit)
+           // customer.CustomerAddress.transfer(msg.value - customer.CarValue - customer.Deposit);
+            return "The car is yours now";
+        }
+        
+        else if(termination==TerminationState.NewSign)
+            customer.StartTime=block.timestamp;
+            customer.CarValue=car[NewCar].CarValue;
+            customer.Duration=NewDuration;
+            customer.MileCap=car[NewCar].MileCap;
+            customer.Loyalty+=1;
+            return "New Sign successfully"; 
     }
+    
+    function ExpireContract () public payable{
+        if(block.timestamp > customer.StartTime + customer.Duration)
+        selfdestruct(customer.CustomerAddress);
+    }
+    
+    
+}
